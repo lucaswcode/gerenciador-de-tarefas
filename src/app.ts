423a0d9c72a5b4976cac2218fe0f165aced76d58
Lucas Willian <lucas.willianxd@gmail.com>
2026-07-26 23:26:58 -0300

    feat: implement exception handling middlewares

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import express from "express";
 
-const app = express();
+import { errorHandling } from "./middlewares/error-handling";
+
+export const app = express();
 
 app.use(express.json());
 
-export { app };
+app.use(errorHandling);
